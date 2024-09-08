c78e437f4cdb5a3783b5ab56384366e9aae76f9a
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-08 15:06:52 +0900

    Add RealityKitContent package and assets

---
+++ Chapter2/Chapter2/Chapter2App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter2App.swift
+//  Chapter2
+//
+//  Created by 木村和貴 on 2024/09/08.
+//
+
+import SwiftUI
+
+@main
+struct Chapter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
